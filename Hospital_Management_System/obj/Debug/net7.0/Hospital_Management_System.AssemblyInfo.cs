0b3b3feef2a4c0542cbde966b4e2731ced7bfb0c
Adel Awad Al Kazzaz <119636436+Adelkazzaz@users.noreply.github.com>
2023-12-22 19:24:16 +0200

    edit login & reg page

--- Hospital_Management_System/obj/Debug/net7.0/Hospital_Management_System.AssemblyInfo.cs
+++ Hospital_Management_System/obj/Debug/net7.0/Hospital_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7869c3ba5db803cd105fc63990d7172231471a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4d61599a4cce3f45921ad65ce092ba4fba0cf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
