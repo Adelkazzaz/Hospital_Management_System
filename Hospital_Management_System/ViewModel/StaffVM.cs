d7cae740548f3bb456ee3d9c98684890800fff8c
Adel Awad Al Kazzaz <119636436+Adelkazzaz@users.noreply.github.com>
2023-12-27 15:52:45 +0200

    Delete Hospital_Management_System directory

--- Hospital_Management_System/ViewModel/StaffVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hospital_Management_System.ViewModel
-{
-	public class StaffVM
-	{
-		public string Name { get; set; }
-		public int  Id { get; set; }
-		public string DeptName { get; set; }
-	}
-}
